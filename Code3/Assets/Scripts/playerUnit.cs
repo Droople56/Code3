08f32bc8598dd73968315b9e27b4105db352e6d8
Storm <31637259+Droople56@users.noreply.github.com>
2020-01-29 18:00:30 -0500

    Firefighter prefab created
    I also made a couple scripts, but they don'e have a whole lot to them.

---
+++ Code3/Assets/Scripts/playerUnit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerUnit : MonoBehaviour
+{
+    //Attributes
+    Vector3 unitPos;
+    Vector3 forward;
+    int health;
+    float speed;
+    //int identifiers for class type 1 = Firefighter / 2 = Paramedic / 3 = Police Officer
+    int unitClass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
