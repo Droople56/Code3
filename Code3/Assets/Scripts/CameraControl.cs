04fb971570b3123822812b46a1a22f5f0bdcafaa
Storm <31637259+Droople56@users.noreply.github.com>
2020-02-04 13:06:12 -0500

    Started Camera Control, Fixed Prefab

---
+++ Code3/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    //Attributes
+    Vector3 position;
+    Vector3 forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.transform.position;
+        //forward = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetButtonDown("w"))
+        {
+            position += forward;
+        }
+        else if (Input.GetButtonDown("A"))
+        {
+
+        }
+        else if (Input.GetButtonDown("S"))
+        {
+
+        }
+        else if (Input.GetButtonDown("D"))
+        {
+
+        }
+    }
+
+    void Rotation()
+    {
+
+    }
+}
