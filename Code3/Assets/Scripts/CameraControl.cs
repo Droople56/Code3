7e30962b948871e2c9227986de6a643e6e126073
Storm <31637259+Droople56@users.noreply.github.com>
2020-02-06 13:17:07 -0500

    Camera Controls w/rotation and Up&Down

--- Code3/Assets/Scripts/CameraControl.cs
+++ Code3/Assets/Scripts/CameraControl.cs
@@ -8,6 +8,9 @@ public class CameraControl : MonoBehaviour
     Vector3 position;
     //this will be forward not relative to the camera, but to the map
     Vector3 forward;
+    Vector3 up;
+
+    Vector3 camLocalEuler;
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +18,17 @@ public class CameraControl : MonoBehaviour
         position = gameObject.transform.position;
         //gameObject.transform.position
         forward = Quaternion.Euler(-30, 0, 0) * gameObject.transform.forward;
+        up = Quaternion.Euler(0, 0, 90) * forward;
+
+        camLocalEuler.x = 30;
+        gameObject.transform.localEulerAngles = camLocalEuler;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
-        gameObject.transform.position = position;
+        Rotation();
     }
 
     void Movement()
@@ -32,24 +39,47 @@ public class CameraControl : MonoBehaviour
             position += forward;
         }
         //left
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             position -= (Quaternion.Euler(0,90,0) * forward);
         }
         //backwards
-        else if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             position -= forward;
         }
         //right
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             position += (Quaternion.Euler(0, 90, 0) * forward);
         }
+        //Up
+        if (Input.GetKey(KeyCode.Equals))
+        {
+            position += (Quaternion.Euler(-90, 0, 0) * forward);
+        }
+        //Down
+        if (Input.GetKey(KeyCode.Minus))
+        {
+            position -= (Quaternion.Euler(-90, 0, 0) * forward);
+        }
+        gameObject.transform.position = position;
     }
 
     void Rotation()
     {
-
+        //Left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            forward = (Quaternion.Euler(0, -3, 0) * forward).normalized;
+            gameObject.transform.forward = forward;
+            gameObject.transform.Rotate(30, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            forward = (Quaternion.Euler(0, 3, 0) * forward).normalized;
+            gameObject.transform.forward = forward;
+            gameObject.transform.Rotate(30, 0, 0);
+        }
     }
 }
