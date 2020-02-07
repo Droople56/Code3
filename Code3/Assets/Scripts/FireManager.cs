71a50a2e05e59fe992d995c12b87ddc03c29719a
dkrsorenson <dkrsorenson@gmail.com>
2020-02-07 16:00:29 -0500

    Modified fire spreading to more accurately randomize the direction and added a spreading fire prefab

--- Code3/Assets/Scripts/FireManager.cs
+++ Code3/Assets/Scripts/FireManager.cs
@@ -11,6 +11,8 @@ public class FireManager : MonoBehaviour
     public GameObject firePrefab;
     private List<GameObject> fires;
 
+    public LayerMask fireLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +38,13 @@ public class FireManager : MonoBehaviour
         if (instantiationTimer <= 0)
         {
             var position = CalculateRandomPosition();
+
+            do
+            {
+                position = CalculateRandomPosition();
+            }
+            while (Physics.OverlapSphere(position, .25f, fireLayer).Length > 0);
+
             var fireObj = Instantiate(firePrefab, position, Quaternion.identity);
             instantiationTimer = 2f;
             fires.Add(fireObj);
@@ -44,8 +53,11 @@ public class FireManager : MonoBehaviour
 
     public Vector3 CalculateRandomPosition()
     {
-        var lastFireObjPosition = fires.Last().transform.position;
-        var randomPosition = lastFireObjPosition + Random.insideUnitSphere * radius;
+        var lastFireObj = fires.Last();
+        var firePosition = lastFireObj.transform.position;
+        var fireRadius = lastFireObj.GetComponentInChildren<ParticleSystem>().shape.radius;
+        var randomDirection = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2)).normalized;
+        var randomPosition = firePosition + (randomDirection * fireRadius) + Random.insideUnitSphere * radius;
         randomPosition.y = 0;
         return randomPosition;
     }
