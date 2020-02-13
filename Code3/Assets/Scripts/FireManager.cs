9c2eb0f64663f07f7d0f0a7eade562ff114ea98c
dkrsorenson <dkrsorenson@gmail.com>
2020-02-12 23:20:48 -0500

    Cleaned up FireManager script (added some comments and updated some logic). Added a spreading fire prefab to the scene.

--- Code3/Assets/Scripts/FireManager.cs
+++ Code3/Assets/Scripts/FireManager.cs
@@ -26,31 +26,35 @@ public class FireManager : MonoBehaviour
         CreateFire();
     }
 
+    /// <summary>
+    /// Starts a fire at the location of this game object.
+    /// </summary>
     void StartFire()
     {
         var newFire = Instantiate(firePrefab, this.transform.position, Quaternion.identity);
         fires.Add(newFire);
     }
 
+    /// <summary>
+    /// Instantiates another instance of the fire prefab at a random location
+    /// </summary>
     void CreateFire()
     {
         instantiationTimer -= Time.deltaTime;
         if (instantiationTimer <= 0)
         {
             var position = CalculateRandomPosition();
-
-            do
-            {
-                position = CalculateRandomPosition();
-            }
-            while (Physics.OverlapSphere(position, .25f, fireLayer).Length > 0);
-
             var fireObj = Instantiate(firePrefab, position, Quaternion.identity);
             instantiationTimer = 2f;
             fires.Add(fireObj);
         }
     }
 
+    /// <summary>
+    /// Calculates a random position for a new fire prefab. 
+    /// Looks for a random position around the last spawned fire prefab.
+    /// </summary>
+    /// <returns>A random position.</returns>
     public Vector3 CalculateRandomPosition()
     {
         var lastFireObj = fires.Last();
@@ -59,6 +63,12 @@ public class FireManager : MonoBehaviour
         var randomDirection = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2)).normalized;
         var randomPosition = firePosition + (randomDirection * fireRadius) + Random.insideUnitSphere * radius;
         randomPosition.y = 0;
+
+        if(Physics.OverlapSphere(randomPosition, .25f, fireLayer).Length > 0)
+        {
+            return CalculateRandomPosition();
+        }
+
         return randomPosition;
     }
 }
