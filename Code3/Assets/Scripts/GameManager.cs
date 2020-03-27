cd3ad874802cf36379133d67d7c8f62ad163be08
Storm <31637259+Droople56@users.noreply.github.com>
2020-03-27 12:04:05 -0400

    Fixed Prefab forwards

--- Code3/Assets/Scripts/GameManager.cs
+++ Code3/Assets/Scripts/GameManager.cs
@@ -43,6 +43,7 @@ public class GameManager : MonoBehaviour
                 else if(hit.transform.gameObject.tag == "Item")
                 {
                     Vector3 dest = new Vector3(hit.point.x, selectedObject.transform.position.y, hit.point.z);
+                    selectedObject.transform.GetComponent<PlayerUnit>().MoveUnit(dest);
                     selectedObject.transform.GetComponent<PlayerUnit>().PickupItem(hit.transform.gameObject);
                 }
                 else
