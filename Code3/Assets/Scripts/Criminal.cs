2da9a6708c4935064315ee737172501977c92657
dkrsorenson <dkrsorenson@gmail.com>
2020-04-03 18:14:06 -0400

    Added criminal with AI to randomly move to different waypoints. Updated navmesh and added navmesh obstacles (houses, trees, civilian cars)

---
+++ Code3/Assets/Scripts/Criminal.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Criminal : MonoBehaviour
+{
+    List<Transform> waypoints;
+    NavMeshAgent agent;
+    [SerializeField]
+    bool reachedPosition = true;
+
+    int currentWaypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        waypoints = new List<Transform>();
+
+        var vehicles = GameObject.FindGameObjectsWithTag("Vehicle").Where(x => x.gameObject.name.Contains("CivilianCar")).Select(x => x.transform).ToList();
+        var houses = GameObject.FindGameObjectsWithTag("House").Select(x => x.transform).ToList();
+
+        waypoints.AddRange(houses);
+        waypoints.AddRange(vehicles);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If he made it to the target position, select a new random waypoint to go to
+        if (reachedPosition)
+        {
+            var randomPosition = Random.Range(0, waypoints.Count);
+            agent.SetDestination(waypoints[randomPosition].position);
+            reachedPosition = false;
+            currentWaypointIndex = randomPosition; 
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if he made it to the current waypoint
+        if(other.gameObject.name.Contains("CivilianCar") || other.gameObject.name.Contains("House")) 
+        {
+            if(waypoints[currentWaypointIndex].position == other.gameObject.transform.position)
+            {
+                reachedPosition = true;
+            }
+        }
+    }
+}
